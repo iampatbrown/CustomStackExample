d7fd67f2ae0cdb2e30a1e4f61befe633b94c2d40
Pat Brown <mrpatbrown@gmail.com>
2022-06-15 20:43:52 +1000

    Added example

--- CustomStackExample/CustomStackExampleApp.swift
+++ CustomStackExample/CustomStackExampleApp.swift
@@ -1,17 +1,83 @@
-//
-//  CustomStackExampleApp.swift
-//  CustomStackExample
-//
-//  Created by Pat Brown on 15/6/2022.
-//
-
+import NavigationBarAppearance
 import SwiftUI
+
+struct ContentView: View {
+  let colors: [Color] = [
+    .red,
+    .green,
+    .orange,
+    .purple,
+    .blue,
+    .pink,
+    .indigo,
+    .black,
+    .brown,
+    .yellow
+  ]
+
+  var body: some View {
+    NavigationView {
+      List(colors, id: \.description, rowContent: ColorRow.init(color:))
+        .listStyle(.plain)
+        .navigationBarTitle("Colors")
+        .navigationBarBackgroundColor(Color(white: 0.95))
+        .navigationBarTitleColor(.black)
+    }
+    .navigationViewStyle(.customStack)
+  }
+}
+
+struct ColorRow: View {
+  var color: Color
+
+  var body: some View {
+    NavigationLink {
+      ColorView(color: color)
+    } label: {
+      HStack {
+        Text(color.description.capitalized)
+        Spacer()
+        Circle()
+          .foregroundColor(color)
+          .frame(width: 60, height: 60)
+          .padding(5)
+      }
+    }
+  }
+}
+
+struct ColorView: View {
+  var color: Color
+
+  var body: some View {
+    VStack {
+      NavigationLink {
+        Text("Details page")
+          .navigationTitle("Details")
+      } label: {
+      Rectangle()
+        .foregroundColor(color)
+        .overlay(Text("Press me!").foregroundColor(.primary))
+        .aspectRatio(1, contentMode: .fit)
+        .padding()
+      }
+
+      Text("\(color.description.capitalized) is an awesome color")
+      Spacer()
+    }
+    .navigationTitle(color.description.capitalized)
+    .navigationBarTitleDisplayMode(.inline)
+    .navigationBarTint(.white)
+    .navigationBarTitleColor(.white)
+    .navigationBarBackgroundColor(color)
+  }
+}
 
 @main
 struct CustomStackExampleApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
