365424e0e92579ada5e0843b6a9eaddcd6724e2a
Pat Brown <mrpatbrown@gmail.com>
2022-06-15 20:24:29 +1000

    Initial Commit

---
+++ CustomStackExample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomStackExample
+//
+//  Created by Pat Brown on 15/6/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomStackExample/CustomStackExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomStackExampleApp.swift
+//  CustomStackExample
+//
+//  Created by Pat Brown on 15/6/2022.
+//
+
+import SwiftUI
+
+@main
+struct CustomStackExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
